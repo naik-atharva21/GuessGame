7e19dd1816a0264edb3a75250c191ab3c71f4cf2
Atharva Naik <atharvanaikofficial@gmail.com>
2021-03-02 00:44:02 +0530

    Added Randomly choosing function

--- GuessGame.java
+++ GuessGame.java
@@ -1,7 +1,31 @@
 class GuessGame
 {
+    private String[] words = 
+    {
+        "Computer",
+        "Earth",
+        "Science",
+        "Goat"
+    }
+
+    String wrd;
+
+    private void choose()
+    {
+        //Choose a random word
+        int index;
+
+        do
+        {
+            index = (int)(math.random() * 10);
+        } while (index > words.length - 1);
+
+        wrd = words[index];
+    }
+
     public static void main(String[] args)
     {
-        
+        GuessGame gg = new GuessGame();
+        gg.choose();
     }
 }
